cec4a42c87d61fba38a7dea96d94395aba12925f
elfeo <elfeo@elfeo-K73SJ>
2015-10-14 18:10:56 +0200

    Bundle

--- src/main/java/fr/homezone/exception/WebApplicationExceptionMapper.java
+++ src/main/java/fr/homezone/exception/WebApplicationExceptionMapper.java
@@ -1,5 +1,7 @@
 package fr.homezone.exception;
 
+import java.util.ResourceBundle;
+
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -9,14 +11,13 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplicationException> {
 
-	// private static final ResourceBundle resource =
-	// ResourceBundle.getBundle("");
+	private static final ResourceBundle resource = 	ResourceBundle.getBundle("fr.homezone.exception.WebApplicationExceptionMapper");
 
 	@Override
 	public Response toResponse(WebApplicationException e) {
 		
 		ErrorMessage errorMessage = new ErrorMessage();
-		
+		errorMessage.setLink(resource.getString(Integer.toString(e.getResponse().getStatus())));
 		errorMessage.setStatus(e.getResponse().getStatus());
 		errorMessage.addMessage(e.getMessage());
 		
