93e7b1bfd3c105ceac318661b6b98d9312933779
elfeo <elfeo@elfeo-K73SJ>
2015-10-14 16:39:45 +0200

    suppression constantes

--- src/main/java/fr/homezone/filter/AppConstants.java
+++
@@ -1,8 +0,0 @@
-package fr.homezone.filter;
-
-public class AppConstants {
-	
-	public static final int GENERIC_APP_ERROR_CODE = 5001;		
-	public static final String BLOG_POST_URL = "http://www.codingpedia.org/ama/tutorial-rest-api-design-and-implementation-in-java-with-jersey-and-spring/";
-
-}
